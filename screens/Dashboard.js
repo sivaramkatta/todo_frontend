8814ae08c25c2efffecacdaaf50c27b9e4640e1d
sivaram004 <katta.sivaram@aasaanjobs.com>
2020-06-04 21:32:43 +0530

    added api call

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import {Text, View, TouchableOpacity, Image, StyleSheet} from 'react-native';
+import Button from '../components/Button';
+import {GET} from '../utils/fetch';
+
+function formatDate(date) {
+  return `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
+}
 
 class Dashboard extends React.Component {
   static navigationOptions = ({navigation}) => {
@@ -17,10 +23,58 @@ class Dashboard extends React.Component {
       ),
     };
   };
+
+  state = {
+    selected: 'TODAY',
+    todos: [],
+  };
+
+  componentDidMount() {
+    this.getTODO();
+  }
+
+  async getTODO(date) {
+    const date_value = date ? date : new Date();
+    const data = await GET(`todo/?date=${formatDate(date_value)}`);
+    if (data.data) {
+      const {data: todos} = data;
+      this.setState({todos});
+    }
+  }
+
+  handleButtonClick(value) {
+    this.setState({selected: value});
+  }
+
   render() {
+    const {selected} = this.state;
     return (
-      <View>
-        <Text>This is dashboard</Text>
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Yesterday"
+            handleOnClick={() => this.handleButtonClick('YESTERDAY')}
+            style={selected === 'YESTERDAY' ? styles.green : {}}
+          />
+          <Button
+            title="Today"
+            handleOnClick={() => this.handleButtonClick('TODAY')}
+            style={selected === 'TODAY' ? styles.green : {}}
+          />
+          <Button
+            title="Tomorrow"
+            handleOnClick={() => this.handleButtonClick('TOMORROW')}
+            style={selected === 'TOMORROW' ? styles.green : {}}
+          />
+          <TouchableOpacity
+            onPress={() => {
+              this.props.navigation.navigate('Search');
+            }}>
+            <View style={styles.search}>
+              <Text style={styles.searchText}>&#x2315;</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -34,5 +88,25 @@ const styles = StyleSheet.create({
     width: 30,
     marginRight: 16,
     backgroundColor: 'white',
+  },
+  container: {
+    margin: 16,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  green: {
+    backgroundColor: 'green',
+  },
+  search: {
+    backgroundColor: '#0b7bfe',
+    paddingHorizontal: 11,
+    paddingVertical: 1,
+    borderRadius: 20,
+  },
+  searchText: {
+    color: 'white',
+    fontSize: 30,
   },
 });
---
+++ screens/Search.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Search = () => <Text>search</Text>;
+
+export default Search;
